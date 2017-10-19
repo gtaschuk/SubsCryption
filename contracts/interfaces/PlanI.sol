674ec821c54182a41619dbc56a310e3b93965abd
Luis Molina <luismol@umich.edu>
2017-10-19 16:15:57 +0400

    Contract interfaces

---
+++ contracts/interfaces/PlanI.sol
@@ -0,0 +1,142 @@
+pragma solidity ^0.4.13;
+
+contract PlanI {
+
+    uint startingPrice;
+    uint floorPrice;
+    uint steepness;
+    uint inflectionPoint;
+    string planDescription;
+
+    uint serviceProviderFunds;
+
+    mapping (address => SubscriberInfo) subscribers;
+
+    struct SubscriberInfo {
+        // 0 -> not signed up
+        // 1 -> inactive
+        // 2 -> active
+        // 3 -> banned
+        uint status;
+
+        //
+        uint balance;
+
+        //
+        uint unwithdrawn;
+
+        // The expiration timestamp for a pre paid subscription.
+        uint payUpfrontExpirationTime;
+
+        // The starting timestamp for the current subscription.
+        uint startingTime;
+    }
+
+    /**
+     * Suscribe a user to a plan.
+     * Set the balance for this user to be the paid amount.
+     *
+     * - Add to the balance
+     */
+    function addBalance() public payable;
+
+    /**
+     * Suscribe a user to a plan.
+     * Pre-pay for a certain amount of time.
+     *
+     * If not active:
+     *  - You set the startingTime to the currentTime
+     *  - You calculate the expirationTime based on the prePayAmount
+     *
+     * If is active:
+     * If startingTime is set:
+     *  - Calcualte the area from startingTime to currentTime
+     *  - Substract this area from the balance
+     *  - Calculate the expirationTime based on prePayAmount
+     * else:
+     *  - You set the startingTime to the currentTime
+     *  - You calculate the expirationTime based on the prePayAmount
+     *
+     * @param timeSpan how long you want to pre pay for
+     */
+    function payUpfront(uint timeSpan) public payable;
+
+    /**
+     * Calculate when the balance expires or until when the user
+     * has paid.
+     * @return the blockNumber when the subscription expires
+     */
+    function calculateExpiration() public constant returns (uint blockNumber);
+
+    /**
+     * Calculates if a user has enough balance to be active or if the
+     * user is in an inactive state.
+     *
+     * Two scenarios:
+     *  1: The user doesn't have an expiration time set.
+     *     - Calculate the area from startingTime to targetTime
+     *     - If balance >= to the area then is active.
+     *  2: The user has an expiration time set.
+     *     - If the targetTime is less than expirationTime then is active
+     *     - Calculate the area from expirationTime to targetTime
+     *     - If balance >= to this area then is active
+     * @return if a user is active or not
+     */
+    function isActive(
+        address subscriber,
+        uint targetTime
+    ) public constant returns (bool isIndeed);
+
+    /**
+     * Get the balance of a user.
+     *
+     * If expirationTime is set and currentTime < expirationTime
+     *  - Return balance
+     * If expirationTime is set and currentTime > expirationTime
+     *  - Calculate area from expirationTime to currentTime
+     *  - Substract the balance from this area
+     * If expirationTime is not set
+     *  - Calculate area from startingTime to currentTime
+     *  - Substract the balance from this area
+     *
+     *
+     * @return the balance of the user
+     */
+    function getBalance() public constant returns (uint balance);
+
+    /**
+     * Get the cost for a prepay time span. Based on your startingTime if active.
+     *
+     * @param timeSpan how much time you want to prepay for?
+     */
+    function getPrepayAmount(
+        address subscriber,
+        uint timeSpan
+    ) public constant returns (uint amount);
+
+    /*
+     * onlySubscriber modifier
+     */
+    function suspendSubscription() public;
+
+    /*
+     * onlyOwner modifier
+     */
+    function banSubscriber()  public;
+
+    /**
+     * Withdraw funds for the plan owner.
+     * MODIFIDER: onlyOwner
+     * These are the funds that have been used for all subscriptions.
+     */
+    function withdrawFunds() public;
+
+    /**
+     * Allow a user to withdraw any unused balance.
+     * - The amount has to be less than the value returned by getBalance()
+     * - Substract amount from balance
+     * @param amount the amount to withdraw
+     * requires the user to have an unused balance.
+     */
+    function withdrawBalance(uint amount) public;
+}
