e7813caad11ddc1488c5e1f825fa4c059880362f
Jason Smythe <jason@smythemail.za.net>
2017-10-20 12:12:42 +0400

    Add template for website widget, add modal.

---
+++ src/containers/UserWidget.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import UserUpdate from './UserUpdate'
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
+import RaisedButton from 'material-ui/RaisedButton'
+
+class UserWidget extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isModalOpen : false
+    }
+  }
+
+  handleOpen = () => {
+    console.log('yesss')
+    this.setState({isModalOpen: true})
+  }
+
+  handleClose = () => {
+    this.setState({isModalOpen: false})
+  }
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onClick={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        disabled={true}
+        onClick={this.handleClose}
+      />,
+    ]
+
+    return (
+      <div>
+        <h1>Your Subscription</h1>
+        <h4>Your Balance:</h4>
+        <h4>Remaining Subscription:</h4>
+        <RaisedButton label="Modal Dialog" onClick={this.handleOpen} />
+        <Dialog
+          actions={actions}
+          modal={true}
+          open={this.state.isModalOpen}
+        >
+          <UserUpdate/>
+        </Dialog>
+      </div>
+    )
+    // title="Dialog With Actions"
+  }
+}
+
+export default UserWidget
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
 import InstanceWrapper from './InstanceWrapper'
+import SpotifyExample from './components/SpotifyExample'
 import App from './App'
 import reducer from './reducers'
 import registerServiceWorker from './registerServiceWorker'
@@ -46,7 +47,8 @@ ReactDOM.render(
             <IndexRoute component={Home} />
             <Route path="dashboard" component={Dashboard} />
           </Route>
-         
+          <Route path="/website" component={SpotifyExample}>
+          </Route>
         </Router>
       </MuiThemeProvider>
     </InstanceWrapper>
